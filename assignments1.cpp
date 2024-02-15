f56229e8ee558d46b3fd706eb333587454c82ae0
Gaurav9810 <160116924+Gaurav9810@users.noreply.github.com>
2024-02-15 15:14:48 +0530

    assignments 1 ans

---
+++ assignments1.cpp
@@ -0,0 +1,35 @@
+// Q1
+#include<iostream>
+using namespace std;
+int main (){
+    cout<<"Physics\n"<<"Wallah";
+}
+
+// // Q2
+#include<iostream>
+using namespace std;
+int main (){
+    int x,y;
+    x=5,y=5;
+    cout<<x+y;
+}
+
+
+// Q3 ans is  1)int=32 bits,2)bool=1byte,3)float=32bits
+// Q4 ans is 6 , 4.
+
+// Q5
+ #include<iostream>
+ using namespace std;
+ int main (){
+     int r,c;  //r = radius , c circumference
+     r = 10;
+     c= 2*3.14*r;
+     cout<<"the circumference of a circle is : "<<c;
+ }
+
+// Q6 ans is 2 
+ 
+
+
+
