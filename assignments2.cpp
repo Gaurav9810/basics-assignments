fa7ac5f4b79ad3d26c5d758c119baea270c4be0b
Gaurav9810 <160116924+Gaurav9810@users.noreply.github.com>
2024-02-17 18:24:44 +0530

    assignments2  answer

---
+++ assignments2.cpp
@@ -0,0 +1,40 @@
+// Q1) Ans is 0
+
+// Q2) Ans is 1,1
+
+// Q3) Ans is 2
+
+// Q4. WAP for finding the volume of the cylinder by taking radius and height as input.
+// Ans 4)
+#include<iostream>
+using namespace std;
+int main(){
+    int r,h,z;
+    cout<<"enter the radius : ";//r = radius
+    cin>>r;
+    cout<<"enter the height : ";//h = height
+    cin>>h;
+    z = 3.14*r*r*h;
+    cout<<"volume of the cylinder is : "<<z;
+}
+
+
+// Q5) WAP to find the difference between ASCII of two characters ,take them as input .
+#include<iostream>
+using namespace std;
+int main(){
+    char x,y;
+    cout<<"enter the first char : ";
+    cin>>x;
+    cout<<"enter the second char : ";
+    cin>>y;
+    int a,b,z;
+    a = int(x);
+    b = int(y);
+    z = a - b;
+    cout<<"the difference between ASCII of two characters is : "<<z;
+}
+
+
+// // Q6) Ana is 49
+
